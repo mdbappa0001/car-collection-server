4e2b4e5f1c426f79df00548ea31e87dbf2915b6f
mdbappa0001 <abutanif366@gmail.com>
2022-05-10 20:39:02 +0600

    added

--- index.js
+++ index.js
@@ -32,12 +32,7 @@ async function run() {
             res.send(service);
         });
 
-            //POST
-            app.post('service', async(req, res) =>{
-            const newService = req.body;
-            const result = await serviceCollection.insertOne(newService);
-            res.send(result);
-        });
+           
 
         //DELETE
         app.delete('/service/:id', async(req, res)=>{
